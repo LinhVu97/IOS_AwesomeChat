b9f34158a3f811ad10448953524573b3bc0ab6f4
LinhVN <linhvn2303@gmail.com>
2021-07-22 11:53:24 +0700

    Setup

---
+++ AwesomeChat/AwesomeChatUITests/AwesomeChatUITests.swift
@@ -0,0 +1,32 @@
+//
+//  AwesomeChatUITests.swift
+//  AwesomeChatUITests
+//
+//  Created by Vũ Linh on 22/07/2021.
+//
+
+import XCTest
+
+class AwesomeChatUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
