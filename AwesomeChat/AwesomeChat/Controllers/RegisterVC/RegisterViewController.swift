f6df45f20dc4588160c27662c43f6907a75410f7
LinhVN <linhvn2303@gmail.com>
2021-07-28 10:35:09 +0700

    Register Screen

--- AwesomeChat/AwesomeChat/Controllers/RegisterVC/RegisterViewController.swift
+++ AwesomeChat/AwesomeChat/Controllers/RegisterVC/RegisterViewController.swift
@@ -8,8 +8,41 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-
+    @IBOutlet weak private var nameText: UITextField!
+    @IBOutlet weak private var emailText: UITextField!
+    @IBOutlet weak private var passText: UITextField!
+    @IBOutlet weak private var registerBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        keyBoard()
+        setupTextField()
+    }
+    
+    // MARK: - Dismiss Keyboard
+    private func keyBoard() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+     
+    // Dismiss Keybroad
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    // MARK: - Setup Text Field.
+    private func setupTextField() {
+        // Name Text
+        let imageNameTextField = UIImage(asset: .user)
+        nameText.iconTextField(image: imageNameTextField ?? UIImage())
+        
+        // Email Text
+        let imageEmailTextField = UIImage(asset: .email)
+        emailText.iconTextField(image: imageEmailTextField ?? UIImage())
+        
+        // Password Text
+        let imagePassTextField = UIImage(asset: .key)
+        passText.iconTextField(image: imagePassTextField ?? UIImage())
+        passText.isSecureTextEntry = true
     }
 }
